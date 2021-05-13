de2527280a16ee9d761c08d4fb712c133da32ede
William <ochowei@gmail.com>
2021-05-13 11:57:14 +0800

    chore: apply systemd nginx service

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ yum install -y epel-release
 yum install -y ansible
 ansible-playbook  /vagrant/playbook.yml  --connection=local --inventory 127.0.0.1,
 cp /vagrant/webapp.service /etc/systemd/system/
-systemctl start webapp
+systemctl enable --now webapp
 cp /vagrant/nginx.conf /etc/nginx/
-nginx
+systemctl enable --now nginx
+
 
