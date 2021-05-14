325e85e07580be04c826ecd2ca13c75dc4f204dd
William <ochowei@gmail.com>
2021-05-14 13:43:12 +0800

    chore: move main folder to /usr/share/uwsgi

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
 yum install -y epel-release
 yum install -y ansible
-ansible-playbook  /vagrant/playbook.yml  --connection=local --inventory 127.0.0.1,
-cp /vagrant/*.service /etc/systemd/system/
+BASEDIR=$(dirname "$0")
+echo $@
+ls
+ansible-playbook  $BASEDIR/playbook.yml  --connection=local --inventory 127.0.0.1,
+cp $BASEDIR/*.service /etc/systemd/system/
 systemctl enable --now uwsgi
-cp /vagrant/nginx.conf /etc/nginx/
+cp $BASEDIR/nginx.conf /etc/nginx/
 systemctl enable --now nginx
 
 
