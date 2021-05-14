1aff63af635b81e35714012ff9d2c0e753424b9d
William <ochowei@gmail.com>
2021-05-14 11:38:37 +0800

    chore: rename service

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 yum install -y epel-release
 yum install -y ansible
 ansible-playbook  /vagrant/playbook.yml  --connection=local --inventory 127.0.0.1,
-cp /vagrant/webapp.service /etc/systemd/system/
-systemctl enable --now webapp
+cp /vagrant/*.service /etc/systemd/system/
+systemctl enable --now uwsgi
 cp /vagrant/nginx.conf /etc/nginx/
 systemctl enable --now nginx
 
