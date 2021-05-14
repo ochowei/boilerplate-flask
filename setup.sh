fd01fe25848bc1f95f174159e3552630cc411828
William <ochowei@gmail.com>
2021-05-14 13:47:55 +0800

    chore: update uid/gid of uwsgi ini

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
 
 yum install -y epel-release
 yum install -y ansible
-BASEDIR=$(dirname "$0")
-echo $@
-ls
+
 ansible-playbook  $BASEDIR/playbook.yml  --connection=local --inventory 127.0.0.1,
+
 cp $BASEDIR/*.service /etc/systemd/system/
 systemctl enable --now uwsgi
 cp $BASEDIR/nginx.conf /etc/nginx/
